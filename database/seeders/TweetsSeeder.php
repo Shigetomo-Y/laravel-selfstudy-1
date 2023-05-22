7f5190da7e734296c9c82ea532d2521b2cf520be
Shigetomo Yamamoto <shigetomo-yamamoto@ShigetomonoMacBook-Pro.local>
2023-05-23 00:42:51 +0900

    new file:   app/Models/Tweet.php 	modified:   config/app.php 	new file:   database/factories/TweetFactory.php 	modified:   database/seeders/TweetsSeeder.php

--- database/seeders/TweetsSeeder.php
+++ database/seeders/TweetsSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tweeet;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
+use App\Models\Tweet;
 
 class TweetsSeeder extends Seeder
 {
@@ -14,10 +14,6 @@ class TweetsSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('tweets')->insert([
-            'content' => Str::random(100),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        Tweet::factory()->count(10)->create();
     }
 }
